9e19616e8d2d71fa5d7bbf39e9ee6b0dbb189afa
Robbe <robbevaes95@gmail.com>
2023-08-10 15:49:17 +0200

    Added favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,6 +16,17 @@ const api = [
 export default defineConfig({
   base: '/forms/',
   title: 'Formango',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#da532c"}],
+    ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#da532c"}],
+    ['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   description: 'A lightweight, zod-based Vue form library',
   themeConfig: {
     search: {
