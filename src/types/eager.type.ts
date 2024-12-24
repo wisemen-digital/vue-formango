0613bb9b96d643d28c4c2b42ef97ee0b281fee5b
Robbe <robbevaes95@gmail.com>
2024-12-24 10:57:02 +0100

    chore: lint and version bump

--- src/types/eager.type.ts
+++ src/types/eager.type.ts
@@ -73,7 +73,7 @@ export type Path<T> = T extends any ? PathInternal<T> : never
 /**
  * See {@link Path}
  */
-export type FieldPath<TFieldValues extends unknown> = Path<TFieldValues>
+export type FieldPath<TFieldValues> = Path<TFieldValues>
 
 /**
  * Helper type for recursively constructing paths through a type.
@@ -172,7 +172,7 @@ export type PathValue<T, P extends Path<T> | ArrayPath<T>> = T extends any
  * See {@link PathValue}
  */
 export type FieldPathValue<
-  TFieldValues extends unknown,
+  TFieldValues,
   TFieldPath extends FieldPath<TFieldValues>,
 > = PathValue<TFieldValues, TFieldPath>
 
