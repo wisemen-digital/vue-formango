ad7e8dba1cf56b2d063e212a8cf43ea8ea5e080e
Robbe95 <robbevaes95@gmail.com>
2025-04-14 11:08:23 +0200

    chore: removed build artifacts

--- src/lib/index.js
+++
@@ -1,2 +0,0 @@
-export { formatErrorsToZodFormattedError } from './formatErrors';
-export { useForm } from './useForm';
--- src/types/index.js
+++
@@ -1,5 +0,0 @@
-export * from './common.type';
-export * from './eager.type';
-export * from './form.type';
-export * from './standardSpec.type';
-export * from './utils.type';
